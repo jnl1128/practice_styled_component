92191f25b3f551c690773dfcec40dde31de48462
narin <nalynnshinhwa1128@gmail.com>
2023-03-05 11:27:41 +0900

    [FEAT] styled component 적용 및 확장예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+    display: flex;
+`;
+const Box = styled.div`
+    background-color: ${props => props.bgColor};
+    width: 100px;
+    height: 100px;
+`;
+const Circle = styled(Box)`
+    border-radius: 50px;
+`;
+const Text = styled.span`
+    color: white;
+`;
+// function App() {
+//     return (
+//         <div style={{ display: "flex" }}>
+//             <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+//             <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+//         </div>
+//     );
+// }
 function App() {
-    return;
+    return (
+        <Father>
+            <Box bgColor="teal">
+                <Text>Hello</Text>
+            </Box>
+            <Circle bgColor="tomato" />
+        </Father>
+    );
 }
 
 export default App;
