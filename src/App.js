f17df706402cfb4c5d061e7a89b474f7b0099919
narin <nalynnshinhwa1128@gmail.com>
2023-03-05 13:14:06 +0900

    [FEAT] as, attrs 예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+    display: flex;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+    background-color: tomato;
+`;
+
 function App() {
-    return;
+    return (
+        <Father as="header">
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+        </Father>
+    );
 }
 
 export default App;
