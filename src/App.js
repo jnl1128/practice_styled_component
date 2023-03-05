97b018b6f9f043ca526f2b2dbda14a20a338cf68
narin <nalynnshinhwa1128@gmail.com>
2023-03-05 14:11:34 +0900

    [FEAT] animations and pseudo selector 예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    /* transform: rotate(360deg); */
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+    font-size: 36px;
+`;
+
+const Box = styled.div`
+    height: 200px;
+    width: 200px;
+    background-color: tomato;
+    animation: ${rotationAnimation} 1s linear infinite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    ${Emoji} {
+        &:hover {
+            font-size: 60px;
+        }
+    }
+`;
+
 function App() {
-    return;
+    return (
+        <Wrapper>
+            <Box>
+                <Emoji as="p">☺️</Emoji>
+            </Box>
+        </Wrapper>
+    );
 }
 
 export default App;
